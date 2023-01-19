f00e29d5e66f9cadee8745635230c8e7ef248824
isyoo1213 <moethns@gmail.com>
2023-01-19 20:12:53 +0900

    feat: Add READ Test of JDBC

--- jdbc/src/main/java/springDB/jdbc/repository/MemberRepositoryV0.java
+++ jdbc/src/main/java/springDB/jdbc/repository/MemberRepositoryV0.java
@@ -5,6 +5,7 @@ import springDB.jdbc.connection.DBConnectionUtil;
 import springDB.jdbc.domain.Member;
 
 import java.sql.*;
+import java.util.NoSuchElementException;
 
 /**
  * JDBC - DriverManager 사용
@@ -20,6 +21,7 @@ public class MemberRepositoryV0 {
         PreparedStatement pstmt = null;
         //Statement를 상속 - parameter Binding할 수 있음
         // + *** SQL Injection 공격을 피하기 위해서 pstmt을 통해 바인딩하는 방식을 추천
+        // * try-catch 때문에 밖에서 null로 초기화해주어야함
 
         try {
             con = getConnection();
@@ -39,6 +41,37 @@ public class MemberRepositoryV0 {
         } finally { // *** 리소스 정리 - 실제 tcp/ip 커넥션을 통해 외부 리소스를 사용하는 것이므로 + 역순으로 진행
             close(con, pstmt, null);
             //현제 rs는 없으므로 null로 전달 - * ResultSet - 쿼리로 결과를 '조회'할 때 사용 (즉, DB로부터 반환값이 있을 때)
+        }
+    }
+
+    public Member findById(String memberId) throws SQLException {
+        String sql = "select * from member where member_id = ?";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+
+            pstmt.setString(1, memberId);
+
+            //SELECT 조회 시, ResultSEt 반환
+            rs = pstmt.executeQuery();
+            if (rs.next()) { //rs에서 최초로 next() 호출 해주어야 *Cursor가 실제 data 접근
+                Member member = new Member();
+                member.setMemberId(rs.getString("member_id"));
+                member.setMoney(rs.getInt("money"));
+                return member;
+            } else {
+                throw new NoSuchElementException("member not found. memberId = " + memberId);
+            }
+        } catch (SQLException e) {
+            log.error("db error = {}", e);
+            throw e;
+        } finally {
+            close(con, pstmt, rs);
         }
     }
 
--- jdbc/src/test/java/springDB/jdbc/repository/MemberRepositoryV0Test.java
+++ jdbc/src/test/java/springDB/jdbc/repository/MemberRepositoryV0Test.java
@@ -1,19 +1,40 @@
 package springDB.jdbc.repository;
 
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import springDB.jdbc.domain.Member;
 
 import java.sql.SQLException;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+@Slf4j
 class MemberRepositoryV0Test {
 
     MemberRepositoryV0 repository = new MemberRepositoryV0();
 
     @Test
     void crud() throws SQLException {
-        Member member = new Member("memberV0", 10000);
+
+        //save
+        Member member = new Member("memberV2", 10000);
         repository.save(member); //save()에 Checked Exception을 throwing하고 있으므로 받아주어야함
+
+        //findById
+        Member findMember = repository.findById(member.getMemberId());
+
+        log.info("findMember = {}", findMember);
+        // @Data의 toString() 메서드가 오버라이딩 됐으므로, 참조값이 아닌 실제값 로깅
+
+        log.info("findMember == member = {}", findMember == member);
+        // *** 실제로는 다른 인스턴스이므로 당연히 false
+        
+        log.info("findMember.equals(member) = {}", findMember.equals(member));
+        // -> but, @Data의 EqualsAndHashCode를 통해 오버라이딩됨 -> 값만 비교할 수 있음
+
+        // -> assertThat()의 isEqualTo() 또한 내부적으로는 비슷한 원리로 값을 비교
+        assertThat(findMember).isEqualTo(member);
     }
 }
