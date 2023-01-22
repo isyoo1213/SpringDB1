64e3a29a865c0607d478979def59f26a55e33213
isyoo1213 <moethns@gmail.com>
2023-01-22 21:27:13 +0900

    feat: Add Test for Connection-Pool

--- jdbc/src/test/java/springDB/jdbc/connection/ConnectionTest.java
+++ jdbc/src/test/java/springDB/jdbc/connection/ConnectionTest.java
@@ -1,5 +1,6 @@
 package springDB.jdbc.connection;
 
+import com.zaxxer.hikari.HikariDataSource;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -38,6 +39,37 @@ class ConnectionTest {
 
         useDataSource(dataSource);
         // -> 결과는 똑같지만, 추상화된 DatasSource 인터페이스를 사용하는 방식
+    }
+
+    @Test
+    void dataSourceConnectionPool() throws SQLException, InterruptedException {
+        //커넥션 풀링
+
+        //HikariDataSource - 이 또한 DataSource를 구현
+        HikariDataSource dataSource = new HikariDataSource();
+
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+        dataSource.setMaximumPoolSize(10);
+        dataSource.setPoolName("MyPool");
+
+        // *** 커넥션을 획득하는 작업은 어플리케이션 실행에 영향을 주지 않기 위해 MyPool connection adder라는 별도의 thread에서 실행
+        // Test실행 시, 별도의 thread에서 실행해 커넥션 풀을 획득하는 과정을 로깅하기 위해 1초정도 슬립을 줌
+        // 슬립을 주지 않으면, test실행 thread의 처리 속도가 빨라 별도의 thread에서 획득한 커넥션 정보가 로그에 전달되지 않음
+        useDataSource(dataSource);
+        Thread.sleep(200);
+
+        //로그 정보
+        //[MyPool housekeeper] ... MyPool - Pool stats (total=7, active=2, idle=5, waiting=0)
+        //3개는 test스레드 완료 후에 실행되어 total 7, 2개는 현재 사용중인 con은 2개, pool에 획득되어 대기상태인 idle은 5
+        // *** Connection Pool에 connection을 채우는 작업은 상대적으로 오래 걸림
+        // -> application 실행 시 대기를 줄이기 위해 별도의 thread 사용
+
+        // + Pool에 1개의 커넥션만 획득한 상태에서 getConnection()호출 시, 나머지 추가적인 con이 pool에 채워지길 기다리는 내부로직 시행
+
+        // + pool의 con 개수가 10개인 상황에서, 10개를 초과하는 getConnection()호출 시
+        // -> 1개의 waiting 발생 -> 30초 후에 연결이 끊겨버림 - 설정을 통해 컨트롤 가능
     }
 
     private void useDataSource(DataSource dataSource) throws SQLException {
