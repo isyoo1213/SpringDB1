d23245afe581b6898c3bc1f5d24b7db65df26625
isyoo1213 <moethns@gmail.com>
2023-01-25 23:28:54 +0900

    feat: Add Comments for MemberServiceV1

--- jdbc/src/main/java/springDB/jdbc/service/MemberServiceV1.java
+++ jdbc/src/main/java/springDB/jdbc/service/MemberServiceV1.java
@@ -16,13 +16,16 @@ public class MemberServiceV1 {
         Member fromMember = memberRepository.findById(fromId);
         Member toMember = memberRepository.findById(toId);
 
+        // 상황 - 계좌1에서의 인출 성공 + 계좌2 검증에서 오류발생 -> 계좌2의 입금까지 로직이 이어지지 않음
         memberRepository.update(fromId, fromMember.getMoney() - money);
 
+        //Ctrl + Alt + M 메서드로 뽑아내기
         validation(toMember);
 
         memberRepository.update(toId, toMember.getMoney() + money);
     }
     private void validation(Member toMember) {
+        //예시를 위한 고의적인 예외 발생
         if (toMember.getMemberId().equals("ex")) {
             throw new IllegalStateException("이체중 예외 발생");
         }
