29f0309401fda00e1e55ad914477e23304b2278f
isyoo1213 <moethns@gmail.com>
2023-02-10 17:52:04 +0900

    feat: Add Test for Throwing Checked Exception

--- jdbc/src/test/java/springDB/jdbc/exception/basic/CheckedTest.java
+++ jdbc/src/test/java/springDB/jdbc/exception/basic/CheckedTest.java
@@ -1,7 +1,10 @@
 package springDB.jdbc.exception.basic;
 
 import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
 
 @Slf4j
 class CheckedTest {
@@ -13,6 +16,14 @@ class CheckedTest {
         // callCatch() 호출 후, catch를 통해 예외 처리를 수행 후 checked_catch() 테스트로 빠져나오고, 로직이 종료됨
         // -> 정상흐름으로 checked_catch() 테스트 메서드가 종료되므로 테스트 성공이 뜨게 됨
     }
+
+    @Test
+    void checked_throw() {
+        Service service = new Service();
+        assertThatThrownBy(() -> service.callThrow()).isInstanceOf(MyCheckedException.class);
+    }
+
+    // * catch와 throws 모두 부모 Exception으로 처리 가능
 
     /**
      * Exception을 상속받은 예외는 Checked 예외가 된다
@@ -41,6 +52,14 @@ class CheckedTest {
                 log.info("예외 처리, message = {}", e.getMessage(), e);
                 //exception을 stackTrace 할 때에는 그냥 객체만 마지막 parameter에 넣어주면 됨
             }
+        }
+
+        /**
+         * checked 예외를 호출한 메서드로 던지는 코드 by throws
+         * 예외처리를 하지 않는 경우 compile에 필수
+         */
+        public void callThrow() throws MyCheckedException {
+            repository.call();
         }
 
     }
